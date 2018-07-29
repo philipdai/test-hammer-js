6cf603ac849ef8f802e138abb675c2e73aee9158
Name From Git-Plugin-Test <pdai@icct.com>
2018-07-29 19:00:19 -0400

    Add task type

--- src/app/tasks/task-type/task-type.component.ts
+++ src/app/tasks/task-type/task-type.component.ts
@@ -1,19 +1,16 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
-import { TaskType } from '../shared/models/task-type.model';
-import { Task } from '../shared/models/task.model';
-
+import { TaskType } from '../../shared/models/task-type.model';
+import { Task } from '../../shared/models/task.model';
 
 @Component({
-  selector: 'task-type',
-  templateUrl: './task-type.component.html',
-  styleUrls: ['./task-type.component.css']
+	selector: 'task-type',
+	templateUrl: './task-type.component.html',
+	styleUrls: [ './task-type.component.css' ],
 })
 export class TaskTypeComponent implements OnInit {
-  ngOnInit() {}
+	ngOnInit() {}
 
-  @Input()
-  taskType: TaskType;
+	@Input() taskType: TaskType;
 
-  @Input()
-  tasks: Task[];
+	@Input() tasks: Task[];
 }
